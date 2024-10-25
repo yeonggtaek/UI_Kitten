0feef5a6d05c9cb480a205f298fa29449b0e85b7
yeongtaek <144389087+yeonggtaek@users.noreply.github.com>
2024-10-24 18:58:36 -0700

    Assclepius

--- app/pages/third.component.js
+++ app/pages/third.component.js
@@ -2,6 +2,7 @@ import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import { Layout, Text, Datepicker, Icon, IconElement } from "@ui-kitten/components";
 
+// ThirdScreen 내에서 사용될 아이콘
 const CalendarIcon = (props): IconElement => (
   <Icon
     {...props}
@@ -9,6 +10,7 @@ const CalendarIcon = (props): IconElement => (
   />
 );
 
+// Datepicker 컴포넌트 추가
 export const DatepickerAccessoriesShowcase = (): React.ReactElement => {
 
   const [date, setDate] = React.useState(new Date());
@@ -30,12 +32,14 @@ export const DatepickerAccessoriesShowcase = (): React.ReactElement => {
   );
 };
 
+// ThirdScreen 컴포넌트
 export const ThirdScreen = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text category='h1'>3</Text>
 
+        {/* Datepicker 추가 */}
         <DatepickerAccessoriesShowcase />
       </Layout>
     </SafeAreaView>
